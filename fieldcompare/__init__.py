fb6898e993aefc07705b816d46a6f926066dc44b
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2022-01-15 20:15:00 +0100

    [io] add readers for json/csv

--- fieldcompare/__init__.py
+++ fieldcompare/__init__.py
@@ -1,3 +1,4 @@
 from .array import Array, make_array, sub_array
 from .field import Field, FieldInterface
 from .predicates import ExactFieldEquality, FuzzyFieldEquality
+from .field_io import read_fields
---
+++ fieldcompare/field_io.py
@@ -0,0 +1,119 @@
+"""Readers of fields from various data formats"""
+
+from multiprocessing.sharedctypes import Value
+from typing import TextIO, Iterable, List
+from os.path import splitext
+from json import load
+from csv import reader
+from numpy import isscalar
+
+from fieldcompare import Field
+
+
+class CSVFieldReader:
+    """Read fields from csv files"""
+
+    def __init__(self, file_stream: TextIO):
+        self._names: list = []
+        self._data: list = []
+
+        csv_reader = reader(file_stream)
+        for row_idx, row in enumerate(csv_reader):
+            row_values = list(row)
+            if row_idx == 0:
+                if not any(_convertible_to_float(v) for v in row_values):
+                    self._names = row_values
+                else:
+                    self._names = [f"field_{i}" for i in range(len(row))]
+                    self._append_data_row(row_values)
+            else:
+                self._append_data_row(row_values)
+
+        # ensure there are no duplicate names
+        assert len(set(self._names)) == len(self._names)
+
+    def field(self, name: str) -> Field:
+        idx = self._names.index(name)
+        if idx >= len(self._names):
+            raise ValueError(f"Could not find the field with name {name}")
+        return Field(name, [row[idx] for row in self._data])
+
+    def field_names(self):
+        return self._names
+
+    def _append_data_row(self, row: list) -> None:
+        self._data.append([self._convert(v) for v in row])
+
+    def _convert(self, input: str):
+        value = _string_to_int(input)
+        if value is not None:
+            return value
+        value = _string_to_float(input)
+        if value is not None:
+            return value
+        return input
+
+
+class JSONFieldReader:
+    """Read fields from json files"""
+
+    def __init__(self, stream: TextIO):
+        self._fields: dict = {}
+        self._load_fields(load(stream))
+
+    def _load_fields(self, data: dict, key_prefix: str = None) -> None:
+        """Read in fields recursively from sub-dictionaries"""
+        for key in data:
+            field_entry_key = f"{key_prefix}/{key}" if key_prefix is not None else key
+            fdata = data[key]
+            if isinstance(fdata, dict):
+                self._load_fields(fdata, field_entry_key)
+            else:
+                fdata = [fdata] if not isinstance(fdata, list) else fdata
+                if not _is_correct_field_data_format(fdata):
+                    raise IOError("Unsupported JSON file layout")
+                self._fields[field_entry_key] = fdata
+
+    def field(self, name: str):
+        for field_name, values in self._fields.items():
+            if field_name == name:
+                return Field(field_name, values)
+        raise ValueError(f"Could not find a field with name {name}")
+
+    def field_names(self):
+        return list(self._fields.keys())
+
+
+def read_fields(filename: str) -> List[Field]:
+    """Read in the fields from the file with the given name"""
+    ext = splitext(filename)[1]
+    with open(filename, "r") as file_stream:
+        if ext == ".json":
+            json_reader = JSONFieldReader(file_stream)
+            return [json_reader.field(name) for name in json_reader.field_names()]
+        if ext == ".csv":
+            csv_reader = CSVFieldReader(file_stream)
+            return [csv_reader.field(name) for name in csv_reader.field_names()]
+    raise NotImplementedError("Unsupported file type")
+
+
+def _is_correct_field_data_format(field_values: Iterable) -> bool:
+    return all(isscalar(value) for value in field_values)
+
+
+def _string_to_int(input: str):
+    try:
+        return int(input)
+    except ValueError:
+        return None
+
+
+def _string_to_float(input: str):
+    try:
+        return float(input)
+    except ValueError:
+        return None
+
+
+def _convertible_to_float(input: str) -> bool:
+    return _string_to_float(input) is not None
