f8ec26aa015169cef9fd984e8eadda8a3670a32e
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2022-01-16 13:25:23 +0100

    [init] add module doc

--- fieldcompare/__init__.py
+++ fieldcompare/__init__.py
@@ -1,3 +1,11 @@
+"""
+The fieldcompare module
+
+Prodives a framework fields of values for relational properties as e.g. equality.
+This can be used e.g. in regression testing simulation software, comparing simulation
+results against previously obtained results.
+"""
+
 from .array import Array, make_array, sub_array
 from .field import Field, FieldInterface
 from .predicates import ExactFieldEquality, FuzzyFieldEquality
