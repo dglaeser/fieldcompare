ac29318ff80e06b8a1736149bdcedc1988d36f7e
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2022-01-16 13:16:57 +0100

    [common] cleanup

--- fieldcompare/_common.py
+++ fieldcompare/_common.py
@@ -1,17 +1,13 @@
 """Common functions and types"""
 
-from numpy import floating as _numpy_float
-from numpy import ndarray as _numpy_array
-from numpy import format_float_scientific as _numpy_format_scientific
-from numpy import printoptions as _numpy_print_options
-from numpy import array2string as _numpy_array_to_string
+import numpy as np
 
 def _get_as_string(obj) -> str:
-    if isinstance(obj, (_numpy_float, float)):
-        return _numpy_format_scientific(obj, unique=True)
-    elif isinstance(obj, _numpy_array):
-        with _numpy_print_options(floatmode="unique") as opts:
-            return _numpy_array_to_string(obj)
+    if isinstance(obj, (np.floating, float)):
+        return np.format_float_scientific(obj, unique=True)
+    if isinstance(obj, np.ndarray):
+        with np.printoptions(floatmode="unique"):
+            return np.array2string(obj)
     return str(obj)
 
 def _default_base_tolerance() -> float:
