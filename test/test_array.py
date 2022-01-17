53b2a39736833aa1fa87b390e92b97bbdbc63912
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2022-01-17 20:16:50 +0100

    [test][array] test fixed-size array construction

--- test/test_array.py
+++ test/test_array.py
@@ -1,10 +1,10 @@
 """Test some helper functions for arrays"""
 
-from multiprocessing.sharedctypes import Value
 from pytest import raises
 
 from context import fieldcompare
 from fieldcompare import make_array, sub_array
+from fieldcompare.array import make_initialized_array, make_uninitialized_array
 
 def test_array_factory_functions():
     arr1 = make_array([0, 1, 2])
@@ -26,6 +26,13 @@ def test_sub_array_extraction():
     with raises(ValueError):
         sub_array((i for i in range(3)), 0, 1)
 
+def test_fixed_size_array_construction():
+    arr1 = make_initialized_array(size=10, init_value=1.0)
+    assert all(v == 1.0 for v in arr1)
+    arr1 = make_uninitialized_array(size=10, dtype="object")
+    assert all(v is None for v in arr1)
+
 if __name__ == "__main__":
     test_array_factory_functions()
     test_sub_array_extraction()
+    test_fixed_size_array_construction()
