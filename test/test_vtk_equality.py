670e964625e6f8a0a58f0bd366ce6038e9058451
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2022-01-17 16:55:54 +0100

    [test][vtk] test non-conforming meshes

--- test/test_vtk_equality.py
+++ test/test_vtk_equality.py
@@ -15,10 +15,12 @@ def _get_field_from_list(name, fields_list):
     return element_list[0]
 
 def _compare_vtk_files(file1, file2, predicate=FuzzyFieldEquality()) -> PredicateResult:
+    print("Comparing vtk files")
     fields1 = read_fields(file1)
     fields2 = read_fields(file2)
     for field1 in fields1:
         field2 = _get_field_from_list(field1.name, fields2)
+        print(f" -- checking field {field1.name}")
         pred_result = predicate(field1, field2)
         if not pred_result:
             return pred_result
@@ -111,7 +113,60 @@ def test_vtk_files_perturbed():
         default_predicate
     )
 
+def test_non_conforming_vtk_files():
+    predicate = FuzzyFieldEquality()
+    default_predicate = DefaultFieldEquality()
+
+    assert _compare_vtk_files(
+        TEST_DATA_PATH / Path("test_non_conforming_mesh.vtu"),
+        TEST_DATA_PATH / Path("test_non_conforming_mesh.vtu"),
+        predicate
+    )
+    assert _compare_vtk_files(
+        TEST_DATA_PATH / Path("test_non_conforming_mesh.vtu"),
+        TEST_DATA_PATH / Path("test_non_conforming_mesh.vtu"),
+        default_predicate
+    )
+
+    assert _compare_vtk_files(
+        TEST_DATA_PATH / Path("test_non_conforming_mesh.vtu"),
+        TEST_DATA_PATH / Path("test_non_conforming_mesh_permutated.vtu"),
+        predicate
+    )
+    assert _compare_vtk_files(
+        TEST_DATA_PATH / Path("test_non_conforming_mesh.vtu"),
+        TEST_DATA_PATH / Path("test_non_conforming_mesh_permutated.vtu"),
+        default_predicate
+    )
+
+    assert _compare_vtk_files(
+        TEST_DATA_PATH / Path("test_non_conforming_mesh.vtu"),
+        TEST_DATA_PATH / Path("test_non_conforming_mesh_permutated_perturbed.vtu"),
+        predicate
+    )
+    assert _compare_vtk_files(
+        TEST_DATA_PATH / Path("test_non_conforming_mesh.vtu"),
+        TEST_DATA_PATH / Path("test_non_conforming_mesh_permutated_perturbed.vtu"),
+        default_predicate
+    )
+
+    predicate.set_absolute_tolerance(1e-20)
+    predicate.set_relative_tolerance(1e-20)
+    default_predicate.set_absolute_tolerance(1e-20)
+    default_predicate.set_relative_tolerance(1e-20)
+    assert not _compare_vtk_files(
+        TEST_DATA_PATH / Path("test_non_conforming_mesh.vtu"),
+        TEST_DATA_PATH / Path("test_non_conforming_mesh_permutated_perturbed.vtu"),
+        predicate
+    )
+    assert not _compare_vtk_files(
+        TEST_DATA_PATH / Path("test_non_conforming_mesh.vtu"),
+        TEST_DATA_PATH / Path("test_non_conforming_mesh_permutated_perturbed.vtu"),
+        default_predicate
+    )
+
 if __name__ == "__main__":
     test_identical_vtk_files()
     test_vtk_files_permutated()
     test_vtk_files_perturbed()
+    test_non_conforming_vtk_files()
