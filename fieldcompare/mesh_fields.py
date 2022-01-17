13c7b61e3a0f9ef87a53bf5b17b3c16e2873e892
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2022-01-17 16:54:07 +0100

    [mesh_fields] uniquely sort duplicate nodes

--- fieldcompare/mesh_fields.py
+++ fieldcompare/mesh_fields.py
@@ -3,27 +3,31 @@
 from typing import Iterable, Tuple, List, Iterator
 
 from ._common import _default_base_tolerance
-from .array import Array, make_array, lex_sort_array_columns
-from .array import min_element, max_element
+from .array import Array, make_array, make_uninitialized_array, append_to_array
+from .array import adjacent_difference, elements_less, accumulate
+from .array import sort_array, lex_sort_array_columns
+from .array import min_element, max_element, abs_array, all_true
 from .field import Field
+
 
 class MeshFields:
     """Stores fields defined on a mesh. Points & cells are sorted to get a unique representation"""
     def __init__(self,
                  points: Array,
                  cells: Iterable[Tuple[str, List]]) -> None:
+        cells = {cell_type: corners for cell_type, corners in cells}
         points = make_array(points)
-        self._point_index_map = _sorting_points_indices(points)
+        self._point_index_map = _sorting_points_indices(points, cells)
         self._fields: list = [
             Field("point_coordinates", points[self._point_index_map])
         ]
 
         point_index_map_inverse = _make_inverse_index_map(self._point_index_map)
         def _map_and_sort_cell_corners(corner_list):
-            return sorted([point_index_map_inverse[c] for c in corner_list])
+            return sort_array(point_index_map_inverse[corner_list])
 
         self._cell_index_maps: dict = {}
-        for cell_type, corners in cells:
+        for cell_type, corners in cells.items():
             for idx, corner_list in enumerate(corners):
                 corners[idx] = _map_and_sort_cell_corners(corner_list)
             self._cell_index_maps[cell_type] = _sorting_cell_indices(corners)
@@ -145,29 +149,99 @@ class TimeSeriesMeshFields:
 
 
 
-def _sorting_points_indices(points) -> Array:
+def _sorting_points_indices(points, cells) -> Array:
     tolerance = _get_point_cloud_tolerance(points)
     def _fuzzy_lt(val1, val2) -> bool:
         return val1 < val2 - tolerance
     def _fuzzy_gt(val1, val2) -> bool:
         return val1 > val2 + tolerance
+    def _fuzzy_lt_point(p1, p2) -> bool:
+        for v1, v2 in zip(p1, p2):
+            if _fuzzy_lt(v1, v2):
+                return True
+            elif _fuzzy_gt(v1, v2):
+                return False
+        return False
 
-    class _FuzzySortHelper:
+    class _IndexedFuzzySortHelper:
         def __init__(self, idx: int) -> None:
             self._idx = idx
-
         def __lt__(self, other) -> bool:
-            my_point = points[self._idx]
-            other_point = points[other._idx]
-            for v1, v2 in zip(my_point, other_point):
-                if _fuzzy_lt(v1, v2):
-                    return True
-                elif _fuzzy_gt(v1, v2):
-                    return False
-            return False
+            return _fuzzy_lt_point(
+                points[self._idx],
+                points[other._idx]
+            )
 
     pre_sort = list(lex_sort_array_columns(points))
-    pre_sort.sort(key=lambda idx: _FuzzySortHelper(idx))
+    pre_sort.sort(key=lambda idx: _IndexedFuzzySortHelper(idx))
+
+    # for non-conforming output, there may be multiple points at the same position
+    # sort those according to the minimum cell center they are connected to
+    def _make_empty_point_connectivity_list() -> Array:
+        arr = make_uninitialized_array(len(points), dtype=list)
+        for idx in range(len(arr)):
+            arr[idx] = []
+        return arr
+
+    cells_around_points = {
+        cell_type: _make_empty_point_connectivity_list()
+        for cell_type, _ in cells.items()
+    }
+    for cell_type, corners in cells.items():
+        for idx, cell_corners in enumerate(corners):
+            for point_index in cell_corners:
+                cells_around_points[cell_type][point_index].append(idx)
+
+    class FuzzyPointSortHelper:
+        def __init__(self, point):
+            self._point = point
+        def __lt__(self, other) -> bool:
+            return _fuzzy_lt_point(self._point, other._point)
+
+    def _compute_cell_center(cell_corner_list):
+        return accumulate(points[cell_corner_list], axis=0)/len(cell_corner_list)
+
+    def _compute_min_cell_center_around_point(point_idx):
+        def _compute_current_cell_center(cell_type, cell_idx):
+            for ct, corners in cells.items():
+                if ct == cell_type:
+                    return _compute_cell_center(corners[cell_idx])
+            raise ValueError("Could not find")
+
+        centers = []
+        for cell_type in cells_around_points:
+            if cells_around_points[cell_type][point_idx] is not None:
+                for cell_index in cells_around_points[cell_type][point_idx]:
+                    centers.append(FuzzyPointSortHelper(
+                        _compute_current_cell_center(cell_type, cell_index)
+                    ))
+        return min(centers)
+
+    diffs = adjacent_difference(points[pre_sort], axis=0)
+    diffs = abs_array(diffs)
+    is_zero_diff = make_array(diffs)
+    is_zero_diff.fill(tolerance)
+    is_zero_diff = elements_less(diffs, is_zero_diff)
+    is_zero_diff = all_true(is_zero_diff, axis=1)
+    is_zero_diff = append_to_array(is_zero_diff, False)
+
+    equal_orig_point_indices = []
+    for list_idx, is_zero in enumerate(is_zero_diff):
+        if is_zero:
+            equal_orig_point_indices.append(list_idx)
+        elif equal_orig_point_indices:
+            # sort chunk of equal points
+            equal_orig_point_indices.append(list_idx)
+            start = equal_orig_point_indices[0]
+            stop = equal_orig_point_indices[-1]
+
+            min_cell_centers = {
+                orig_pidx: _compute_min_cell_center_around_point(pre_sort[orig_pidx])
+                for orig_pidx in equal_orig_point_indices
+            }
+            equal_orig_point_indices.sort(key=lambda orig_pidx: min_cell_centers[orig_pidx])
+            pre_sort[start:stop+1] = [pre_sort[orig_pidx] for orig_pidx in equal_orig_point_indices]
+            equal_orig_point_indices = []
     return make_array(pre_sort)
 
 
@@ -184,7 +258,7 @@ def _get_point_cloud_tolerance(points):
 
 
 def _make_inverse_index_map(forward_map: Array) -> Array:
-    inverse = make_array([0]*len(forward_map))
+    inverse = make_array(forward_map)
     for list_index, mapped_index in enumerate(forward_map):
         inverse[mapped_index] = list_index
     return inverse
