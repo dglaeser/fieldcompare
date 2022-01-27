3531127f38e0bae523f354a41759f9da4d34b306
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2022-01-27 14:44:34 +0100

    [logging] add small logging class

---
+++ fieldcompare/logging.py
@@ -0,0 +1,48 @@
+"""Interface & implementations for loggers"""
+
+from sys import stdout
+from typing import TextIO, Optional
+from abc import ABC, abstractmethod
+
+class Logger(ABC):
+    """Interface for loggers"""
+    def __init__(self, verbosity_level: int = None) -> None:
+        self._verbosity_level = verbosity_level
+
+    @property
+    def verbosity_level(self) -> Optional[int]:
+        return self._verbosity_level
+
+    @verbosity_level.setter
+    def verbosity_level(self, level: int) -> None:
+        self._verbosity_level = level
+
+    def log(self, message: str, verbosity_level: int = None) -> None:
+        if self._verbosity_level is None:
+            self._log(message)
+        elif verbosity_level is None and self._verbosity_level > 0:
+            self._log(message)
+        elif verbosity_level is not None and verbosity_level <= self._verbosity_level:
+            self._log(message)
+
+    @abstractmethod
+    def _log(self, message: str) -> None:
+        """Implementation-defined message logging"""
+
+
+class StreamLogger(Logger):
+    """Logging into output streams"""
+    def __init__(self,
+                 ostream: TextIO,
+                 verbosity_level: int = None) -> None:
+        self._ostream = ostream
+        Logger.__init__(self, verbosity_level)
+
+    def _log(self, message: str) -> None:
+        self._ostream.write(message)
+
+
+class StandardOutputLogger(StreamLogger):
+    """Logging to standard out"""
+    def __init__(self, verbosity_level: int = None) -> None:
+        StreamLogger.__init__(self, stdout, verbosity_level)
