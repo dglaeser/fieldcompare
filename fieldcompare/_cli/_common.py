68d8dac6a9f5750ea35397287cdfda49596b4ec7
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2022-01-28 11:38:12 +0100

    [cli] log file_io operations

--- fieldcompare/_cli/_common.py
+++ fieldcompare/_cli/_common.py
@@ -5,13 +5,15 @@ from textwrap import indent
 
 from .._common import _style_text, TextStyle, TextColor
 from ..compare import ComparisonLog, SkipLog
-from ..logging import Logger
+from ..logging import Logger, ModifiedVerbosityLoggerFacade, IndentedLoggingFacade
 from ..field_io import read_fields
 
 
 def _read_fields_from_file(filename: str, logger: Logger):
     logger.log(f"Reading fields from '{filename}'\n", verbosity_level=1)
-    return read_fields(filename)
+    low_verbosity_logger = ModifiedVerbosityLoggerFacade(logger, verbosity_change=-2)
+    file_io_logger = IndentedLoggingFacade(low_verbosity_logger, " "*4)
+    return read_fields(filename, logger=file_io_logger)
 
 
 def _get_status_string(passed: bool) -> str:
