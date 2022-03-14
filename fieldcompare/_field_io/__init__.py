49d916a7cd00493bff73ac5d496001ae43a4f798
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2022-03-14 11:43:57 +0100

    [field-io] remove json support
    json files are not a natural file format to store field data of the kind
    that we focus on here, which can be seen from the complexity and
    limitations of the JSONFieldReader deleted with this commit.

--- fieldcompare/_field_io/__init__.py
+++ fieldcompare/_field_io/__init__.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from . import _csv, _json, _mesh_io
+from . import _csv, _mesh_io
 from ._reader_map import FieldReader
 from ._reader_map import _EXTENSION_TO_READER_MAP
 
