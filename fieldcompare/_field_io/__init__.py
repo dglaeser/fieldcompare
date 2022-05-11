30f085caceba0de6633d24e4cbb49d3a0762cea0
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2022-05-11 20:50:12 +0200

    [field_io] move reader_getter to map file

--- fieldcompare/_field_io/__init__.py
+++ fieldcompare/_field_io/__init__.py
@@ -2,8 +2,4 @@ from typing import Optional
 
 from . import _csv, _mesh_io
 from ._reader_map import FieldReader
-from ._reader_map import _EXTENSION_TO_READER_MAP
-
-
-def _get_reader_for_extension(extension: str) -> Optional[FieldReader]:
-    return _EXTENSION_TO_READER_MAP.get(extension)
+from ._reader_map import _EXTENSION_TO_READER_MAP, _get_reader_for_extension
--- fieldcompare/_field_io/_reader_map.py
+++ fieldcompare/_field_io/_reader_map.py
@@ -1,14 +1,20 @@
 """A map from file extensions to suitable readers, to be filled by reader implementations."""
 
-from typing import Iterable, Protocol
+from typing import Iterable, Protocol, Optional
 from ..field import FieldInterface
 from ..logging import Loggable
-
-_EXTENSION_TO_READER_MAP: dict = {}
 
 class FieldReader(Loggable, Protocol):
     def read(self, filename: str) -> Iterable[FieldInterface]:
         ...
+
+
+_EXTENSION_TO_READER_MAP: dict = {}
+
+
+def _get_reader_for_extension(extension: str) -> Optional[FieldReader]:
+    return _EXTENSION_TO_READER_MAP.get(extension)
+
 
 def _register_reader_for_extension(extension: str, reader: FieldReader) -> None:
     _EXTENSION_TO_READER_MAP[extension] = reader
