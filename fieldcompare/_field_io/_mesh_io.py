c5845a4c0ec13e338c78f169923fda76f84f7887
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2022-05-13 12:04:10 +0200

    [_meshio] centralize field naming logic

--- fieldcompare/_field_io/_mesh_io.py
+++ fieldcompare/_field_io/_mesh_io.py
@@ -1,6 +1,7 @@
 """Reader for mesh file formats using meshio under the hood"""
 
 from typing import Iterator, List, Tuple, Callable
+from enum import Enum, auto
 from os.path import splitext
 from dataclasses import dataclass
 from xml.etree import ElementTree
@@ -18,6 +19,11 @@ from ._mesh import Mesh, DefaultMesh, TransformedMesh, TransformedMeshBase
 from ._mesh import transform_identity, transform_without_ghosts, transform_sorted
 
 
+class _MeshData(Enum):
+    point_data = auto()
+    cell_data = auto()
+
+
 class _TimeSeriesFieldContainer:
     """
         A field container that contains the fields of multiple time steps.
@@ -27,13 +33,15 @@ class _TimeSeriesFieldContainer:
     @dataclass
     class _FieldInfo:
         name: str
-        data_type: str
+        data_type: _MeshData
 
     @dataclass
     class _TimestepData:
         point_data: dict
         cell_data: dict
         step_idx: int
+
+    _time_step_field_name_suffix = "_timestep_"
 
     def __init__(self,
                  time_series_reader: _MeshIOTimeSeriesReader,
@@ -73,25 +81,26 @@ class _TimeSeriesFieldContainer:
 
     def _get_field(self, field_info: _FieldInfo) -> Field:
         name, data_type = field_info.name, field_info.data_type
-        if name == "point_coordinates":
+        if name == _point_coordinates_field_name():
             return Field(name, self._transformed_mesh.points)
-        elif name.endswith("_corners") and data_type == "cell_data":
-            cell_type = name.rsplit("_corners")[0]
+        elif name.endswith(_cell_corners_suffix()) \
+                and data_type == _MeshData.cell_data:
+            cell_type = name.rsplit(_cell_corners_suffix())[0]
             return Field(name, self._transformed_mesh.connectivity[cell_type])
-        elif data_type == "point_data":
+        elif data_type == _MeshData.point_data:
             return self._get_point_data_field(name)
-        elif data_type == "cell_data":
+        elif data_type == _MeshData.cell_data:
             return self._get_cell_data_field(name)
         raise ValueError("Unknown data type")
 
     def _get_point_data_field(self, name: str) -> Field:
-        base_name = name.split("_timestep_")[0]
+        base_name = name.rsplit(self._time_step_field_name_suffix)[0]
         field_data = self._timestep_data.point_data[base_name]
         field_data = self._transformed_mesh.transform_point_data(field_data)
         return Field(name, field_data)
 
     def _get_cell_data_field(self, name: str) -> Field:
-        base_name_with_cell_type = name.split("_timestep_")[0]
+        base_name_with_cell_type = name.rsplit(self._time_step_field_name_suffix)[0]
         base_name, cell_type = self._split_cell_type_suffix(base_name_with_cell_type)
         for cell_block, cell_data in zip(self._meshio_mesh.cells,
                                          self._timestep_data.cell_data[base_name]):
@@ -103,38 +112,40 @@ class _TimeSeriesFieldContainer:
     def _split_cell_type_suffix(self, name: str) -> Tuple[str, str]:
         for cell_type in self._transformed_mesh.connectivity:
             if name.endswith(cell_type):
-                return name.rsplit(f"_{cell_type}")[0], cell_type
+                return _remove_cell_type_suffix(name, cell_type), cell_type
         raise ValueError("Did not find a cell type suffix on the given name")
 
     def _find_fields_of_all_timesteps(self) -> List[_FieldInfo]:
-        fields = []
-        fields.append(self._FieldInfo("point_coordinates", "point_data"))
+        fields = [self._FieldInfo(_point_coordinates_field_name(), _MeshData.point_data)]
         for cell_type in self._transformed_mesh.connectivity:
-            fields.append(self._FieldInfo(f"{cell_type}_corners", "cell_data"))
+            fields.append(self._FieldInfo(
+                _make_cell_corners_field_name(cell_type),
+                _MeshData.cell_data
+            ))
         for step in range(self._time_series_reader.num_steps):
             fields.extend(self._find_fields_of_step(step))
         return fields
 
     def _find_fields_of_step(self, step_idx: int) -> List[_FieldInfo]:
         fields = []
-        for element in list(self._time_series_reader.collection[step_idx]):
-            if element.tag == "Attribute":
-                fields.extend(self._get_element_field_infos(step_idx, element))
+        collection = self._time_series_reader.collection[step_idx]
+        for element in filter(lambda _e: _e.tag == "Attribute", collection):
+            fields.extend(self._get_element_field_infos(step_idx, element))
         return fields
 
     def _get_element_field_infos(self, step_idx: int, xdmf_element) -> List[_FieldInfo]:
         if xdmf_element.get("Center") == "Node":
-            return [self._FieldInfo(
-                self._make_step_field_name(step_idx, xdmf_element.get("Name")),
-                "point_data"
-            )]
+            field_name = xdmf_element.get("Name")
+            field_name = self._make_step_field_name(step_idx, field_name)
+            return [self._FieldInfo(field_name, _MeshData.point_data)]
         elif xdmf_element.get("Center") == "Cell":
-            name = xdmf_element.get("Name")
-            return [
-                self._FieldInfo(
-                    self._make_step_field_name(step_idx, f"{name}_{cell_type}"),
-                    "cell_data"
+            field_name = xdmf_element.get("Name")
+            def _make_name(cell_type):
+                return self._make_step_field_name(
+                    step_idx, _make_cell_type_field_name(field_name, cell_type)
                 )
+            return [
+                self._FieldInfo(_make_name(cell_type), _MeshData.cell_data)
                 for cell_type in self._transformed_mesh.connectivity
             ]
 
@@ -208,9 +219,9 @@ class MeshIOFieldReader(LoggableBase):
         transformed_mesh = self._transform(mesh)
 
         fields = []
-        fields.append(Field("point_coordinates", transformed_mesh.points))
+        fields.append(Field(_point_coordinates_field_name(), transformed_mesh.points))
         for cell_type, corners in mesh.connectivity.items():
-            fields.append(Field(f"{cell_type}_corners", corners))
+            fields.append(Field(_make_cell_corners_field_name(cell_type), corners))
         for name in _meshio_mesh.point_data:
             fields.append(Field(
                 f"{name}",
@@ -221,7 +232,7 @@ class MeshIOFieldReader(LoggableBase):
         for name in _meshio_mesh.cell_data:
             for cell_block, values in zip(_meshio_mesh.cells, _meshio_mesh.cell_data[name]):
                 fields.append(Field(
-                    f"{name}_{cell_block.type}",
+                    _make_cell_type_field_name(name, cell_block.type),
                     transformed_mesh.transform_cell_data(cell_block.type, values)
                 ))
         return DefaultFieldContainer(fields)
@@ -274,6 +285,30 @@ class MeshIOFieldReader(LoggableBase):
 def _register_readers_for_extensions(register_function: Callable[[str, MeshIOFieldReader], None]) -> None:
     for ext in _meshio_supported_extensions:
         register_function(ext, MeshIOFieldReader())
+
+
+def _cell_corners_suffix() -> str:
+    return "_corners"
+
+
+def _point_coordinates_field_name() -> str:
+    return "point_coordinates"
+
+
+def _cell_type_suffix(cell_type: str) -> str:
+    return f"_{cell_type}"
+
+
+def _remove_cell_type_suffix(field_name: str, cell_type: str) -> str:
+    return field_name.rsplit(f"{_cell_type_suffix(cell_type)}")[0]
+
+
+def _make_cell_type_field_name(field_name: str, cell_type: str) -> str:
+    return f"{field_name}_{cell_type}"
+
+
+def _make_cell_corners_field_name(cell_type: str) -> str:
+    return f"{cell_type}{_cell_corners_suffix()}"
 
 
 def _convert_meshio_mesh(mesh: _MeshIO_Mesh) -> Mesh:
