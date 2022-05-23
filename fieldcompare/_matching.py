289506b28fdd46f331cb3a8fb578a0c49d55b7c9
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2022-05-23 09:04:05 +0200

    [matching] make string match find function public

--- fieldcompare/_matching.py
+++ fieldcompare/_matching.py
@@ -15,8 +15,8 @@ class MatchResult:
     orphan_references: List[str]
 
 
-def _find_matching_names(result_names: Iterable[str],
-                         reference_names: Iterable[str]) -> MatchResult:
+def find_matching_names(result_names: Iterable[str],
+                        reference_names: Iterable[str]) -> MatchResult:
     res_orphans = list(set(result_names).difference(reference_names))
     ref_orphans = list(set(reference_names).difference(result_names))
     matches = list(set(result_names).intersection(reference_names))
@@ -26,7 +26,7 @@ def _find_matching_names(result_names: Iterable[str],
 def find_matching_field_names(result_fields: Iterable[FieldInterface],
                               reference_fields: Iterable[FieldInterface]) -> MatchResult:
     """Looks for matching field names in the provided results & reference fields"""
-    return _find_matching_names(
+    return find_matching_names(
         [f.name for f in result_fields],
         [f.name for f in reference_fields]
     )
@@ -35,7 +35,7 @@ def find_matching_field_names(result_fields: Iterable[FieldInterface],
 def find_matching_file_names(results_folder: str,
                              references_folder: str) -> MatchResult:
     """Looks for matching supported files in a results & reference folder to be compared"""
-    return _find_matching_names(
+    return find_matching_names(
         _find_sub_files_recursively(results_folder),
         _find_sub_files_recursively(references_folder)
     )
