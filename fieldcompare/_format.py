80feddc2afb4f79d2a376edfcffa296d1e787516
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2022-10-10 10:18:16 +0200

    [_format] add as_success and highlight

--- fieldcompare/_format.py
+++ fieldcompare/_format.py
@@ -3,13 +3,13 @@
 from typing import List
 from textwrap import indent
 
-from ._colors import make_colored, TextColor
+from ._colors import make_colored, TextColor, TextStyle
 
 
 def get_status_string(passed: bool) -> str:
     if passed:
-        return make_colored("PASSED", color=TextColor.green)
-    return make_colored("FAILED", color=TextColor.red)
+        return as_success("PASSED")
+    return as_error("FAILED")
 
 
 def as_warning(text: str) -> str:
@@ -20,5 +20,17 @@ def as_error(text: str) -> str:
     return make_colored(text, color=TextColor.red)
 
 
-def make_list_string(missing_fields: List[str]) -> str:
-    return indent("\n".join(missing_fields), "- ")
+def as_success(text: str) -> str:
+    return make_colored(text, color=TextColor.green)
+
+
+def highlight(text: str) -> str:
+    return make_colored(text, style=TextStyle.bright)
+
+
+def make_list_string(names: List[str]) -> str:
+    return indent("\n".join(names), "- ")
+
+
+def make_indented_list_string(names: List[str]) -> str:
+    return indent(make_list_string(names), "  ")
