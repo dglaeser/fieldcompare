96ef276e7924d44c9d96a185bbf00a183d817a79
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2022-12-08 21:22:59 +0100

    [mesh] expose sub-package classes

---
+++ fieldcompare/mesh/__init__.py
@@ -0,0 +1,5 @@
+"""Classes and functions related to fields defined on computational meshes"""
+
+from ._mesh import Mesh
+from ._permuted_mesh import PermutedMesh
+from ._mesh_fields import MeshFields, PermutedMeshFields
