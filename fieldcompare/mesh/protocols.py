99b0f568af43ad4b22cbc8713e366adf9db3d5bb
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2022-12-10 09:22:43 +0100

    [mesh] add mesh fields protocol

--- fieldcompare/mesh/protocols.py
+++ fieldcompare/mesh/protocols.py
@@ -1,6 +1,7 @@
 """Defines the interfaces used by the fieldcompare.mesh module"""
-from typing import Protocol, Iterable, runtime_checkable
+from typing import Protocol, Iterable, Tuple, runtime_checkable
 from .._array import Array
+from ..protocols import FieldData, Field
 
 
 @runtime_checkable
@@ -19,4 +20,28 @@ class Mesh(Protocol):
 
     def connectivity(self, cell_type: str) -> Array:
         """Return the corner indices array for the cells of the given type"""
+        ...
+
+
+@runtime_checkable
+class MeshFields(FieldData, Protocol):
+    """Represents fields defined on a computational mesh"""
+    @property
+    def domain(self) -> Mesh:
+        """Return the mesh on which the fields are defined"""
+        ...
+
+    @property
+    def point_fields(self) -> Iterable[Field]:
+        """Return an range over the contained point fields"""
+        ...
+
+    @property
+    def cell_fields(self) -> Iterable[Field]:
+        """Return an range over the contained cell fields"""
+        ...
+
+    @property
+    def cell_fields_types(self) -> Iterable[Tuple[Field, str]]:
+        """Return a range over cell fields + associated cell type"""
         ...
