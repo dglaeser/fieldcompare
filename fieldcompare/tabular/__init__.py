2486a7d02a738d4cd5e76f01c8bdbcd583112355
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2022-12-12 15:16:00 +0100

    [tabular] expose queries on supported file types

--- fieldcompare/tabular/__init__.py
+++ fieldcompare/tabular/__init__.py
@@ -4,9 +4,19 @@ from ._tabular_fields import Table, TabularFields
 from ._csv_field_reader import CSVFieldReader
 
 
+def is_tabular_data_file(filename: str) -> bool:
+    """Return true if the given file contains (supported) tabular data"""
+    return splitext(filename)[1] == ".csv"
+
+
+def is_tabular_data_sequence(filename: str) -> bool:
+    """Return true if the tabular data file contains a sequence of field data"""
+    return False
+
+
 def read(filename: str, delimiter: str = ",", use_names: bool = True) -> TabularFields:
     """Read the tabular data from the given file"""
-    ext = splitext(filename)
+    ext = splitext(filename)[1]
     if ext == ".csv":
         return CSVFieldReader(delimiter=delimiter, use_names=use_names).read(filename)
-    raise NotImplementedError(f"Tabular data reading for files with extension {ext}")
+    raise NotImplementedError(f"No support for tabular data I/O for files with extension '{ext}'")
