43759ddd7d951e73ea797e5ea6bba476483ff0bc
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2022-12-16 00:02:50 +0100

    [io][vtu] use our own i/o mechanism

--- fieldcompare/mesh/_io.py
+++ fieldcompare/mesh/_io.py
@@ -1,85 +1,33 @@
 """I/O mechanisms for fields defined on computational meshes"""
-from os.path import splitext
-from xml.etree import ElementTree
-
-from meshio import read as _meshio_read
-from meshio import Mesh as MeshIOMesh
-from meshio import extension_to_filetypes as _meshio_supported_extensions
-from meshio.xdmf import TimeSeriesReader as MeshIOTimeSeriesReader
-
 from .._field_sequence import FieldDataSequence
 from ._mesh_fields import MeshFields
-from .meshio_utils import from_meshio
+from ._vtk import is_supported as is_supported_vtk, read as read_vtk
+from . import meshio_utils
 
 
 def is_mesh_file(filename: str) -> bool:
     """Return true if the given file is a (supported) mesh file"""
-    return splitext(filename)[1] in _meshio_supported_extensions
+    if is_supported_vtk(filename):
+        return True
+    return meshio_utils._is_supported(filename)
 
 
 def is_mesh_sequence(filename: str) -> bool:
-    """Return true if the given file contains a mesh sequence"""
-    if splitext(filename)[1] not in [".xmf", ".xdmf"]:
-        return False
-
-    parser = ElementTree.XMLParser()
-    tree = ElementTree.parse(filename, parser)
-    root = tree.getroot()
-    if root.tag != "Xdmf":
-        return False
-    for domain in filter(lambda d: d.tag == "Domain", root):
-        for grid in filter(lambda g: g.tag == "Grid", domain):
-            if grid.get("GridType") == "Collection" \
-                    and grid.get("CollectionType") == "Temporal":
-                return True
-    return False
+    """Return true if the given file contains a (supported) mesh sequence"""
+    return meshio_utils._is_supported_sequence(filename)
 
 
 def read(filename: str) -> MeshFields:
     """Read the fields from the given mesh file"""
-    if not is_mesh_file(filename):
-        raise IOError(f"Unsupported mesh file '{filename}'")
-    try:
-        return from_meshio(_meshio_read(filename))
-    except Exception as e:
-        raise IOError(f"Error reading mesh file: {e}")
+    if is_supported_vtk(filename):
+        return read_vtk(filename)
+    if meshio_utils._is_supported(filename):
+        return meshio_utils._read(filename)
+    raise IOError(f"Unsupported mesh file '{filename}'")
 
 
 def read_sequence(filename: str) -> FieldDataSequence:
     """Read a sequence from the given filename"""
-    ext = splitext(filename)[1]
-    if ext not in [".xmf", ".xdmf"]:
-        raise IOError(f"Unsupported mesh sequence file: '{filename}'")
-
-    try:
-        return FieldDataSequence(source=_XDMFSequenceSource(filename))
-    except Exception as e:
-        raise IOError(f"Error reading mesh sequence: {e}")
-
-
-class _XDMFSequenceSource:
-    def __init__(self, filename: str) -> None:
-        self._meshio_reader = MeshIOTimeSeriesReader(filename)
-        self._mesh = from_meshio(
-            MeshIOMesh(*self._meshio_reader.read_points_cells())
-        ).domain
-        self._step_idx = 0
-
-    def reset(self) -> None:
-        self._step_idx = 0
-
-    def step(self) -> bool:
-        self._step_idx += 1
-        return self._step_idx < self._meshio_reader.num_steps
-
-    def get(self) -> MeshFields:
-        _, point_data, cell_data = self._meshio_reader.read_data(self._step_idx)
-        return MeshFields(
-            mesh=self._mesh,
-            point_data=point_data,
-            cell_data=cell_data
-        )
-
-    @property
-    def number_of_steps(self) -> int:
-        return self._meshio_reader.num_steps
+    if meshio_utils._is_supported_sequence(filename):
+        return meshio_utils._read_sequence(filename)
+    raise IOError(f"Unsupported sequence file '{filename}'")
--- fieldcompare/mesh/_vtk/__init__.py
+++ fieldcompare/mesh/_vtk/__init__.py
@@ -1,11 +1,33 @@
 from os.path import splitext
 
+from ..._field_sequence import FieldDataSequence
 from .._mesh_fields import MeshFields
 from ._vtu_reader import VTUReader
 
 
+_VTK_MESH_EXTENSIONS_TO_READER = {
+    ".vtu": VTUReader
+}
+
+
 def read(filename: str) -> MeshFields:
+    """Read mesh fields from the given VTK file"""
     ext = splitext(filename)[1]
-    if ext == ".vtu":
-        return VTUReader(filename).read()
-    raise NotImplementedError(f"Unsupported VTK file extension '{ext}'")
+    if ext not in _VTK_MESH_EXTENSIONS_TO_READER:
+        raise IOError(f"Unsupported VTK file extension '{ext}'")
+    return _VTK_MESH_EXTENSIONS_TO_READER[ext](filename).read()
+
+
+def read_sequence(filename: str) -> FieldDataSequence:
+    """Read a sequence from a VTK file"""
+    raise IOError(f"Unsupported VTK sequence file '{filename}'")
+
+
+def is_supported(filename: str) -> bool:
+    """Return true if the given VTK file is supported"""
+    return splitext(filename)[1] in _VTK_MESH_EXTENSIONS_TO_READER
+
+
+def is_supported_sequence(filename: str) -> bool:
+    """Return true if the given VTK sequence file is supported"""
+    return False
