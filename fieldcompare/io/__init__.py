a3d6ff2692db30e7cacb64740d4a0b3a967ea1b6
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2022-12-19 10:42:42 +0100

    [io] add csv file reader

--- fieldcompare/io/__init__.py
+++ fieldcompare/io/__init__.py
@@ -1,13 +1,17 @@
 """I/O facilities to read field data from files"""
 
+from os.path import splitext
 from typing import Union
 from .. import protocols
 
 from . import vtk
+from ._csv_reader import CSVFieldReader
 
 
 def read(filename: str) -> Union[protocols.FieldData, protocols.FieldDataSequence]:
     """Read the field data from the given file"""
     if vtk.is_supported(filename):
         return vtk.read(filename)
+    if splitext(filename)[1] == ".csv":
+        return CSVFieldReader().read(filename)
     raise IOError(f"Unsupported file '{filename}'")
