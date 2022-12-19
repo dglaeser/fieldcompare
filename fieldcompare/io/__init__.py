4616e63ce265193cd80c8bb0e64b0d5da3911d52
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2022-12-19 11:55:25 +0100

    [io] add query if file is supported

--- fieldcompare/io/__init__.py
+++ fieldcompare/io/__init__.py
@@ -6,7 +6,11 @@ from .. import protocols
 
 from . import vtk
 from ._csv_reader import CSVFieldReader
-from ._mesh_io import _read as _meshio_read, _HAVE_MESHIO
+from ._mesh_io import (
+    _read as _meshio_read,
+    _is_supported as _supported_by_meshio,
+    _HAVE_MESHIO
+)
 
 
 def read(filename: str) -> Union[protocols.FieldData, protocols.FieldDataSequence]:
@@ -15,7 +19,7 @@ def read(filename: str) -> Union[protocols.FieldData, protocols.FieldDataSequenc
         return vtk.read(filename)
     if splitext(filename)[1] == ".csv":
         return CSVFieldReader().read(filename)
-    if _HAVE_MESHIO:
+    if _HAVE_MESHIO and _supported_by_meshio(filename):
         try:
             return _meshio_read(filename)
         except IOError as e:
@@ -25,3 +29,10 @@ def read(filename: str) -> Union[protocols.FieldData, protocols.FieldDataSequenc
         "" if _HAVE_MESHIO else " (consider installing 'meshio' to have access to more file formats)"
         )
     )
+
+
+def is_supported(filename: str) -> bool:
+    """Return true if the given file is supported for field-I/O"""
+    return vtk.is_supported(filename) \
+        or splitext(filename)[1] == ".csv" \
+        or _supported_by_meshio(filename)
