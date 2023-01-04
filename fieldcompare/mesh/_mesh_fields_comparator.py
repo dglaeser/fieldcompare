d105910b72b3f9eb65694669973176e1f7739be4
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2023-01-04 17:04:18 +0100

    [mesh][comparator] add option for space dimension matching

--- fieldcompare/mesh/_mesh_fields_comparator.py
+++ fieldcompare/mesh/_mesh_fields_comparator.py
@@ -4,7 +4,12 @@ from typing import Callable
 
 from ..predicates import DefaultEquality
 from . import protocols as mesh_protocols
-from ._transformations import strip_orphan_points, sort_points, sort_cells
+from ._transformations import (
+    strip_orphan_points,
+    sort_points,
+    sort_cells,
+    extend_space_dimension_to
+)
 from .._field_data_comparison import (
     FieldDataComparator,
     PredicateSelector,
@@ -22,17 +27,24 @@ class MeshFieldsComparator:
     Args:
         source: Mesh field data to be compared.
         reference: Reference mesh field data to compare against.
+        disable_mesh_reordering: If true, no attempt is made to pass the domain equality check with reordered meshes.
         disable_orphan_point_removal: If true, unconnected points are included in reordered meshes.
+        disable_space_dimension_matching: If true, not attempt is made to pass the domain equality
+                                          check by making the space dimensions of the two meshes match.
         field_inclusion_filter: Filter to select the fields to be compared (optional).
         field_exclusion_filter: Filter to exclude fields from being compared (optional).
     """
     def __init__(self,
                  source: mesh_protocols.MeshFields,
                  reference: mesh_protocols.MeshFields,
+                 disable_mesh_reordering: bool = False,
                  disable_orphan_point_removal: bool = False,
+                 disable_space_dimension_matching: bool = False,
                  field_inclusion_filter: Callable[[str], bool] = lambda _: True,
                  field_exclusion_filter: Callable[[str], bool] = lambda _: False) -> None:
+        self._disable_mesh_reordering = disable_mesh_reordering
         self._disable_orphan_point_removal = disable_orphan_point_removal
+        self._disable_space_dimension_matching = disable_space_dimension_matching
         self._source = source
         self._reference = reference
         self._field_inclusion_filter = field_inclusion_filter
@@ -55,28 +67,53 @@ class MeshFieldsComparator:
             reordering_callback: Function that is invoked with status messages about the reordering
                                  steps that are performed. Default: no-op lambda.
         """
-        suite = self._make_comparator()(predicate_selector, fieldcomp_callback)
+        suite = self._run_comparison(predicate_selector, fieldcomp_callback)
         if suite.domain_equality_check:
             return suite
 
-        reordering_callback("Meshes did not compare equal ({}). Retrying with sorted points...".format(
-            suite.domain_equality_check.report
-        ))
-        def _permute(mesh_fields):
-            if not self._disable_orphan_point_removal:
-                mesh_fields = strip_orphan_points(mesh_fields)
-            return sort_points(mesh_fields)
-        self._source = _permute(self._source)
-        self._reference = _permute(self._reference)
-        suite = self._make_comparator()(predicate_selector, fieldcomp_callback)
-        if suite.domain_equality_check:
-            return suite
+        # (maybe) retry with matching space dimension
+        space_dim_source = self._source.domain.points.shape[1]
+        space_dim_target = self._reference.domain.points.shape[1]
+        if space_dim_source != space_dim_target and not self._disable_space_dimension_matching:
+            reordering_callback(self._mesh_fail_msg(suite.domain_equality_check.report, "extended points"))
+            max_dim = max(space_dim_source, space_dim_target)
+            self._source = extend_space_dimension_to(max_dim, self._source)
+            self._reference = extend_space_dimension_to(max_dim, self._reference)
+            suite = self._run_comparison(predicate_selector, fieldcomp_callback)
+            if suite.domain_equality_check:
+                return suite
 
-        reordering_callback("Meshes did not compare equal ({}). Retrying with sorted cells...".format(
-            suite.domain_equality_check.report
-        ))
-        self._source = sort_cells(self._source)
-        self._reference = sort_cells(self._reference)
+        # (maybe) retry with sorted meshes
+        if not self._disable_mesh_reordering:
+            # sorted points
+            reordering_callback(self._mesh_fail_msg(suite.domain_equality_check.report, "sorted points"))
+            def _permute(mesh_fields):
+                if not self._disable_orphan_point_removal:
+                    mesh_fields = strip_orphan_points(mesh_fields)
+                return sort_points(mesh_fields)
+            self._source = _permute(self._source)
+            self._reference = _permute(self._reference)
+            suite = self._run_comparison(predicate_selector, fieldcomp_callback)
+            if suite.domain_equality_check:
+                return suite
+
+            # sorted cells
+            reordering_callback(self._mesh_fail_msg(suite.domain_equality_check.report, "sorted cells"))
+            self._source = sort_cells(self._source)
+            self._reference = sort_cells(self._reference)
+            suite = self._run_comparison(predicate_selector, fieldcomp_callback)
+        if not suite.domain_equality_check:
+            reordering_callback(self._mesh_fail_msg(suite.domain_equality_check.report))
+        return suite
+
+    def _mesh_fail_msg(self, predicate_report: str, retry_measure: str = "") -> str:
+        return f"Meshes did not compare equal ({predicate_report})" + (
+            f". Retrying with {retry_measure}..." if retry_measure else ""
+        )
+
+    def _run_comparison(self,
+                        predicate_selector: PredicateSelector,
+                        fieldcomp_callback: FieldComparisonCallback) -> FieldComparisonSuite:
         return self._make_comparator()(predicate_selector, fieldcomp_callback)
 
     def _make_comparator(self) -> FieldDataComparator:
