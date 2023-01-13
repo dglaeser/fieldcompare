25f379e3d988636ea353b6bd77209dc66950bc7e
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2023-01-13 10:35:01 +0100

    [io][csv] do not extract fields as 1d-vectors

--- fieldcompare/io/_csv_reader.py
+++ fieldcompare/io/_csv_reader.py
@@ -26,7 +26,7 @@ class CSVFieldReader:
             skip_header=self._skip_rows,
             dtype=None,
             encoding="UTF-8",
-            ndmin=2,
+            ndmin=1,
         )
 
         if self._is_structured(data):
@@ -36,9 +36,12 @@ class CSVFieldReader:
                 domain=Table(num_rows=data.shape[0]),
                 fields={name: data[name] for name in data.dtype.names},  # type: ignore
             )
+
         return TabularFields(
             domain=Table(num_rows=data.shape[0]),
-            fields={f"field_{i}": data[:, i] for i in range(data.shape[1])},
+            fields={"field_0": data[:]}
+            if len(data.shape) == 1
+            else {f"field_{i}": data[:, i] for i in range(data.shape[1])},
         )
 
     def _is_structured(self, array: np.ndarray) -> bool:
