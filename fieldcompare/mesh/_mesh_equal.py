ef65ff0618497d101926e95433534d938c26ac19
Dennis Gläser <dennis.a.glaeser@gmail.com>
2024-01-10 22:36:32 +0100

    [mesh_equal] add support for compatible cell types

--- fieldcompare/mesh/_mesh_equal.py
+++ fieldcompare/mesh/_mesh_equal.py
@@ -3,9 +3,11 @@
 
 """Equality check for computational meshes"""
 from __future__ import annotations
+from itertools import product
 
 from .._numpy_utils import Array, make_array, get_sorting_index_map
 from ..predicates import FuzzyEquality, ExactEquality, PredicateResult
+from ._cell_type import CellType
 from .protocols import Mesh as MeshInterface
 
 
@@ -21,14 +23,19 @@ def mesh_equal(
     points_equal = FuzzyEquality(rel_tol=rel_tol, abs_tol=abs_tol)(source.points, target.points)
     if not points_equal:
         return PredicateResult(False, report=f"Differing points - '{points_equal.report}'")
-    if set(source.cell_types) != set(target.cell_types):
+
+    source_ct, target_ct = set(source.cell_types), set(target.cell_types)
+    diff = source_ct.difference(target_ct).union(target_ct.difference(source_ct))
+    if len(_without_compatibles(diff)) != 0:
         return PredicateResult(False, report="Differing grid cell types detected")
+
     for cell_type in source.cell_types:
-        if len(source.connectivity(cell_type)) != len(target.connectivity(cell_type)):
-            return PredicateResult(False, report=f"Differing number of cells of type '{cell_type.name}'")
+        tct = cell_type if cell_type in target_ct else _find_compatible(target_ct, cell_type)
+        if len(source.connectivity(cell_type)) != len(target.connectivity(tct)):
+            return PredicateResult(False, report=f"Differing number of cells of types '{cell_type.name}/{tct}'")
         if not ExactEquality()(
             _get_sorted_corner_indices(source.connectivity(cell_type)),
-            _get_sorted_corner_indices(target.connectivity(cell_type)),
+            _get_sorted_corner_indices(target.connectivity(tct)),
         ):
             return PredicateResult(False, report=f"Differing connectivity detected for cell of type '{cell_type.name}'")
     return PredicateResult(True)
@@ -55,3 +62,18 @@ def _get_fixed_size_corner_indices_sorted(corners: Array) -> Array:
 
 def _get_dynamic_size_corner_indices_sorted(corners: Array) -> Array:
     return make_array([c[get_sorting_index_map(c)] for c in corners], dtype="object")
+
+
+def _without_compatibles(cts: set[CellType]) -> set[CellType]:
+    to_remove: set[CellType] = set()
+    for c1, c2 in product(cts, cts):
+        if c1.is_compatible_with(c2):
+            to_remove = to_remove.union(set([c1, c2]))
+    return cts.difference(to_remove)
+
+
+def _find_compatible(cts: set[CellType], ct: CellType) -> CellType:
+    for c in cts:
+        if c.is_compatible_with(ct):
+            return c
+    raise RuntimeError("Could not find compatible cell type")
